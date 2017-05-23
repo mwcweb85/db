458e0f5dd20c8a9cbd7f26ca18e4c9c2806d6fed
mwcweb1 <mwcweb1@outlook.com>
2017-05-23 13:29:41 +0200

    u

---
+++ db/db/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="db.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Repeater ID="Repeater_navn" runat="server">
+                <ItemTemplate>
+                    <%# Eval("navn") %><br />
+                </ItemTemplate>
+            </asp:Repeater>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:db %>" SelectCommand="SELECT * FROM [Navn]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ db/db/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace db
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter ada = new SqlDataAdapter("select * from Navn", conn);
+            DataTable dt = new DataTable();
+            ada.Fill(dt);
+            Repeater_navn.DataSource = dt;
+            Repeater_navn.DataBind();
+        }
+    }
+}
